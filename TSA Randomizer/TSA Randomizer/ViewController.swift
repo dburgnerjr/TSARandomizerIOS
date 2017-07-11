5172deb6ff28cf6ae00f8be95132906dbe1a9af5
Daniel Burgner <dburgnerjr@yahoo.com>
2017-07-10 21:45:56 -0400

    Added Buttons
    Added buttons to ViewController

--- TSA Randomizer/TSA Randomizer/ViewController.swift
+++ TSA Randomizer/TSA Randomizer/ViewController.swift
@@ -15,6 +15,38 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func aboutButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Coming soon!", message:
+            "To this app.", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style:
+            UIAlertActionStyle.default, handler:nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func startButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Coming soon!", message:
+            "To this app.", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style:
+            UIAlertActionStyle.default, handler:nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func shareButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Coming soon!", message:
+            "To this app.", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style:
+            UIAlertActionStyle.default, handler:nil))
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    @IBAction func removeAdsButton(_ sender: Any) {
+        let alertController = UIAlertController(title: "Coming soon!", message:
+            "To this app.", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style:
+            UIAlertActionStyle.default, handler:nil))
+        present(alertController, animated: true, completion: nil)
+    }
+ 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
