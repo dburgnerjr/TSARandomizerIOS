1dcce057fa5f37d759f3065cc4e854d1e4eb8086
Daniel Burgner <dburgnerjr@yahoo.com>
2017-07-12 22:12:34 -0400

    Start screen complete.
    Completed Start screen.  Was unable to replicate the same functionality
    on the Android app (when “click here” button is clicked, it disappears
    and random arrow appears in its place for a brief time before button
    reappears).

--- TSA Randomizer/StartScreen.swift
+++ TSA Randomizer/StartScreen.swift
@@ -10,13 +10,28 @@ import UIKit
 
 class StartScreen: UIViewController {
     
+    @IBOutlet weak var arrowView: UIImageView!
+    @IBOutlet weak var clickHereButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        clickHereButton.addTarget(self, action: #selector(StartScreen.buttonClicked(_:)), for: .touchUpInside)
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+    }
+    
+    func buttonClicked(_ sender: Any) {
+        //clickHereButton.isHidden = true
+        let nRandom = Int(arc4random_uniform(2))
+        if (nRandom == 0) {
+            arrowView.image = #imageLiteral(resourceName: "Left_Arrow")
+        } else {
+            arrowView.image = #imageLiteral(resourceName: "Right_Arrow")
+        }
+        //clickHereButton.isHidden = false
     }
     
     @IBAction func backButton(_ sender: Any) {
